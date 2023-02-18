23bf4f4eeffed3bcb2ae3ba8284d42801d8a4259
Usama Zahid <usamabutt8080@gmail.com>
2023-02-19 00:18:55 +0500

    project added

---
+++ script.js
@@ -0,0 +1,95 @@
+//1st Problem:
+var admin = "Umair Azmat"
+var name = "Umair Azmat"
+alert(admin);
+
+//2nd Problem:
+var planet = 'earth';
+console.log(planet);
+
+//3rd Problem:
+var currentVisitor = 'Umair Azmat';
+console.log(currentVisitor);
+
+//4th Problem:
+var favDrink = 'coca cola';
+console.log(favDrink);
+console.log(typeof favDrink);
+
+//5th Problem:
+var favNum = 19;
+console.log(favNum);
+console.log(typeof favNum);
+
+//6th Problem:
+function myFavs(){
+    var color = 'red';
+    var food = 'Biryani';
+    var movie = 'F9';
+    console.log('My favourite color' + color + ".");
+    console.log('My favourite food' + food + ".");
+    console.log('My favourite movie' + movie + ".");
+}
+myFavs();
+
+//7th Problem:
+var person = [name = 'Umair' , age =20];
+console.log(person);
+console.log(typeof person);
+
+//8th Problem:
+var a = 20;
+var b = 30;
+var c = a+b;
+console.log(c);
+
+//9th problem:
+var a =50;
+var b = 2;
+var c = a/b;
+console.log(c);
+
+//10th Problem:
+var a = 50;
+var b = 2;
+var c = a+b*5;
+console.log(c);
+
+//11 Problem:
+var F = 50;
+var C = (F-32)*5/9;
+console.log(C);
+
+//12th Problem:
+var a = parseFloat(prompt('Enter Your First Number'));
+var b = parseFloat(prompt('Enter Your Second Number'));
+var c = a+b;
+alert(c);
+
+//13 Problem:
+var a = prompt('Enter Your Num');
+var b = prompt('Enter Your Num');
+if(a>b){
+    alert('A is greater than b');
+}else{
+    alert('A is smaller than b');   
+}
+
+//14th Problem:
+var a = parseFloat(prompt("Enter your number"));
+var b = parseFloat(prompt('Enter your number'));
+var c = a*b;
+alert(c);
+
+//15th Problem:
+var a = parseFloat(prompt("Enter your number"));
+var b = parseFloat(prompt('Enter your number'));
+var c = a/b;
+alert(c);
+console.log(c);
+
+//16th Problem:
+var a = parseFloat(prompt("Enter your number"));
+var b = parseFloat(prompt('Enter your number'));
+var c = a%b;
+alert(c);
